e6b8d11e2b9f68569866c2115d3fcfcdeae0089b
Ivan <i.s.senkin@yandex.ru>
2021-05-10 18:26:48 +0300

    Добавьте файлы проекта.

---
+++ EX1.b/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace EX1.b
+{
+    //б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса.
+    //в) Добавить диалог с использованием switch демонстрирующий работу класса.
+    class Complex
+    {
+       public double re;
+       public double im;
+       public Complex Minus(Complex x)
+        {
+            Complex y = new Complex();
+            y.im = x.im - this.im;
+            y.re = x.re - this.re;
+            return y;
+        }
+        public Complex Multi(Complex x)
+        {
+            Complex y = new Complex();
+            y.im = x.im * this.im;
+            y.re = x.re * this.re;
+            return y;
+        }
+        public string Print() 
+        {
+            return re + "+(" + im + ")i";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex x = new Complex();
+            x.re = 1;
+            x.im = 1;
+            Complex y = new Complex();
+            y.re = 2;
+            y.im = 2;
+            Complex result2 = y.Multi(x);
+            Complex result1 = y.Minus(x);
+
+            while (true)
+            {
+                Console.WriteLine("Число 1: " + x.Print() + "\nЧисло 2: " + y.Print());
+                Console.WriteLine($"Выберите действие \n 0 - выход \n 1 - умножить \n 2 - вычесть");
+                int dialog = int.Parse(Console.ReadLine());
+                if (dialog == 0) break;
+                switch (dialog)
+                {
+                    case 1:
+                        Console.WriteLine(result2.Print());
+                        break;
+                    case 2:
+                        Console.WriteLine(result1.Print());
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ EX2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //С клавиатуры вводятся числа, пока не будет введён 0(каждое число в новой строке).
+            // Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse.
+            double b, summ = 0;
+            string a;
+            while(true)
+            {
+                Console.WriteLine("Введите число: ");
+                a = Console.ReadLine();
+
+                bool result = double.TryParse(a, out b);
+                if (result == true)
+                {
+                    if (b > 0 && b % 2 != 0)
+                    {
+                        summ = b + summ;
+                    }
+                    else if (b == 0)
+                        {
+                        break;
+                    }
+                  
+                } else
+                {
+                    Console.WriteLine("Ошибка, вы ввели не число");
+                }
+
+                } 
+            
+            
+            Console.WriteLine($"сумма нечетных чисел: {summ}");
+        }
+    }
+}
